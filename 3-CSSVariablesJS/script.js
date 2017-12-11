e9893535ded5401da9a5973f891d2c6439f0e21f
Jerome <pisi.jerome@gmail.com>
2017-12-11 16:24:02 +0100

    Variables CSS & JS + Array Cardio training

---
+++ 3-CSSVariablesJS/script.js
@@ -0,0 +1,18 @@
+
+//Sélection de tous les inputs
+const inputs = document.querySelectorAll('section input');
+
+function upDate()
+{
+	//récupération de l'unité de mesure grâc à notre data-sizing.
+	const suffixe = this.dataset.sizing || '';
+	//Redéfinition des varaibles dans la balise html. Elles s'appliqueront partout selon le principe de cascade.
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffixe);
+	//console.log(this.name);
+
+}
+
+//Ecoutes des inputs au changement et au passage de la souris.
+//Combinaison forEach + Function fléchées ES6 function input() {input.addEvent...}
+inputs.forEach(input => input.addEventListener('change', upDate));
+inputs.forEach(input => input.addEventListener('mousemove', upDate));
